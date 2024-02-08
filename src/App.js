3fadaafcd64f77e0b0dd069c41123ec7aa632b00
krishnateja-81 <kteja8212@gmail.com>
2024-02-08 07:46:58 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -37,15 +37,15 @@ function App() {
         <div>
           <h1>Rock Paper Scissors</h1>
           <center>
-            <a href="/" className="btn btn-primary btn-lg" >
+            <a href="/" className="btn btn-danger btn-lg" >
               Reset
             </a>
           </center>
         </div>
         <Router>
           <Routes>
-          <Route path="/" element={(Home())} />
-            <Route path="analyze" element={<Win />} />
+          <Route path="/" exact element={(Home())} />
+            <Route path="analyze" exact element={<Win />} />
           </Routes>
         </Router>
       </div>
